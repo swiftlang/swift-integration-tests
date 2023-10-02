bc6ce67f0535e8482cb8d32fffcb9732ee4b1f90
Yuta Saito <kateinoigakukun@gmail.com>
2023-10-02 10:26:05 -0700

    Add integration tests for static-linking of Foundation (#87)
    Implementation: https://github.com/apple/swift-corelibs-foundation/pull/2996 and https://github.com/apple/swift/pull/35936

--- test-foundation-package/test-foundation-networking.swift
+++ test-foundation-package/test-foundation-networking.swift
@@ -4,3 +4,8 @@ import FoundationNetworking
 let url = URL(string: "http://example.com")!
 let urlRequest = URLRequest(url: url)
 print(urlRequest)
+
+let urlSession = URLSession.shared
+let task = urlSession.dataTask(with: urlRequest)
+print(task.state)
+task.cancel()
